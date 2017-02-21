63b41de0f0db78b1d33a5ac2996e1e19d49ee042
haltaction <haltaction@gmail.com>
2017-02-21 18:13:20 +0200

    Added Security, simple user login/logout, template changes

---
+++ src/RedmineBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RedmineBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * Render and check user login
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('RedmineBundle:Security:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
